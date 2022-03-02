ddda70ebb76583381837ce8a361907b23d3a7e21
Ramy Alfred <ramyalfred@Ramys-MacBook-Pro.local>
2022-03-02 14:29:03 +0200

    Completed Guess My Number game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -5,21 +5,35 @@ const GUESS_RANGE = 20;
 const STARTING_SCORE = 20;
 const NO_INPUT_MESSAGE = '🚨 You haven\'t entered a number';
 const CORRECT_GUESS_MESSAGE = '✅ Right guess!';
-const HIGH_MESSAGE = '📈 Lower!'
-const LOW_MESSAGE = '📉 Higher!'
+const HIGH_MESSAGE = '📈 Lower!';
+const LOW_MESSAGE = '📉 Higher!';
+const INITIAL_MESSAGE = 'Start guessing...';
 
+//Global Variables
+let randomNumber;
+let score;
+let highScore = 0;
 
 //Select a random number to be guessed
-
 function initializeGame(){
-  const randomNumber = Math.round(Math.random() * GUESS_RANGE);
-  const score = STARTING_SCORE;
-}
+  randomNumber = Math.round(Math.random() * GUESS_RANGE);
+  score = STARTING_SCORE;
+  document.querySelector('.score').textContent = STARTING_SCORE;
+  document.querySelector('body').style.backgroundColor = null;
+  document.querySelector('body').style.width = null;
+  setMessage(INITIAL_MESSAGE);
+  document.querySelector('.number').textContent = '?';
+};
 
-
+initializeGame();
 
 function setMessage(msg){
   document.querySelector('.message').textContent = msg;
+}
+
+function decrementScore(){
+  score --;
+  document.querySelector('.score').textContent = score;
 }
 
 //Checking  a guess
@@ -40,19 +54,23 @@ document.querySelector('.check').addEventListener('click',      function() {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
+    highScore = score > highScore? score : highScore;
+    document.querySelector('.highscore').textContent = highScore;
+
   //When guess is higher  
   }else if (userGuess > randomNumber){
     setMessage(HIGH_MESSAGE);
-
+    decrementScore();
+  //When guess is lower
   }else if (userGuess < randomNumber){
     setMessage(LOW_MESSAGE);
-
+    decrementScore();
   }
 })
 
 // Resetting the game
 document.querySelector('.again').addEventListener('click',
 function() {
-  
+  initializeGame();
 }
 )
