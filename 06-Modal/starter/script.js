8f237781105cb2cf0757481a952f11f4761f1e97
ramyalfred <ramy.alfred92@gmail.com>
2022-03-02 15:54:01 +0200

    Completing logic for Modals excercise

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const modalHideButton = document.querySelector('.close-modal');
+const modalObject = document.querySelector('.modal');
+
+//Show Modal
+function showModal(){
+  document.querySelector('.modal').classList.remove('hidden');
+  document.querySelector('.overlay').classList.remove('hidden');
+}
+
+//Hide Modal
+function hideModal(){
+  document.querySelector('.modal').classList.add('hidden');
+  document.querySelector('.overlay').classList.add('hidden');
+}
+
+//Having buttons trigger modal visibility functions
+const modalShowButtonsList = document.querySelectorAll('.show-modal');
+
+modalShowButtonsList.forEach(
+  function(node){
+    console.log(node.textContent);
+    node.addEventListener('click',showModal);
+  }
+);
+
+//Implementing the Modal close button
+modalHideButton.addEventListener('click',
+  function(){
+    hideModal();
+  }
+);
+
+document.querySelector('.overlay').addEventListener('click', 
+  function(){
+    hideModal();
+  }
+)
