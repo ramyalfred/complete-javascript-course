294daf4f90fcff27d9a70610ee01c0567fc6dcb8
Ramy Alfred <ramy.alfred92@gmail.com>
2022-03-19 14:15:17 +0200

    Starting the functions coding challenge

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer(){
+    const answer = prompt(`${this.question}\n${this.options}`);
+    return;
+  },
+};
+
+console.log(poll.registerNewAnswer());
